48878b7b6dcce868530f813c410ccbb863eaabf4
Đinh Nguyễn Nhật Tùng <dnntung@gmail.com>
2022-03-04 15:57:31 +0700

    feat: Week3, Lab1
    - Re-arrange folder structure for practice exercises and weekly exercises

---
+++ Week3/imageModel.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+String jsonRaw =
+    '{ "id": 1, "url": "https://via.placeholder.com/600/92c952&quot;"}';
+
+class ImageModel {
+  int? id;
+  String? url;
+
+  ImageModel(this.id, this.url);
+
+  ImageModel.fromJson(jsonObject) {
+    id = jsonObject['id'];
+    url = jsonObject['url'];
+  }
+
+  String toString() {
+    return '($id, $url)';
+  }
+}
+
+void main() {
+  // print(jsonRaw);
+  var jsonObject = json.decode(jsonRaw);
+
+  // print(jsonObject['id']);
+  // print(jsonObject['url']);
+
+  var image1 = ImageModel(jsonObject['id'], jsonObject['url']);
+  var image2 = ImageModel.fromJson(jsonObject);
+  print(image2);
+}
