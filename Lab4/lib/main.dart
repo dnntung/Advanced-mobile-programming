5a487d5779835d622c2bbee31eb4035f337ab21d
touri-nomi <nhattung1709@gmail.com>
2022-03-21 07:49:00 +0700

    style(lab4): Top app bar

--- Lab4/lib/main.dart
+++ Lab4/lib/main.dart
@@ -13,18 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'messenger_ui_clone',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme:
+          ThemeData(primarySwatch: Colors.blue, backgroundColor: Colors.black),
       home: const Chats(
         title: "Chats",
       ),
--- Lab4/lib/views/chats.dart
+++ Lab4/lib/views/chats.dart
@@ -20,6 +20,11 @@ class Chats extends StatefulWidget {
 }
 
 class _ChatsState extends State<Chats> {
+  static const avatarUrl =
+      "https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-6/240120172_1018783688953234_6482622079438608942_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=aiLx1ymJy2wAX835e7D&_nc_ht=scontent.fsgn5-8.fna&oh=00_AT-R81wkKDob6y7i-RBO3PUl9y6qHnraYL9i-CWmU3yV3g&oe=623C1619";
+
+  _onAvatarPressed() => {};
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -29,6 +34,19 @@ class _ChatsState extends State<Chats> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: const Text("Chats"),
+          titleTextStyle:
+              const TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+          toolbarHeight: 80,
+          leading: IconButton(
+              onPressed: _onAvatarPressed,
+              icon: const CircleAvatar(
+                backgroundImage: NetworkImage(avatarUrl),
+              ))),
       body: Column(
         children: [],
       ),
@@ -41,6 +59,7 @@ class _ChatsState extends State<Chats> {
         selectedLabelStyle: const TextStyle(color: Colors.blue),
         items: const [
           BottomNavigationBarItem(
+            backgroundColor: Colors.transparent,
             icon: Icon(Icons.chat_bubble),
             label: 'Chats',
           ),
